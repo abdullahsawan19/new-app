10c195699ebd2e6855702cc8947e1ed77a65f6e2
Abdullah Sawan <abdullahsawan19@gmail.com>
2025-11-13 23:52:07 +0200

    faqs show

---
+++ src/Accordion.jsx
@@ -0,0 +1,10 @@
+import AccordionItem from "./AccordionItem";
+export default function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
+    </div>
+  );
+}
